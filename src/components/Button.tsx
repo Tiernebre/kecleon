00d96226026f2c8c4417b398bd4feb5655476ddc
Brendan Tierney <tiernebre@gmail.com>
2021-06-17 22:20:02 -0600

    cleanup?

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,5 @@
-import { Fragment   } from "react";
+import { Fragment } from "react";
 
 export const Button = (): JSX.Element => {
-  return (
-      <button>Hi, it's Kecleon!</button>
-  );
+  return <button>Hi, it's Kecleon!</button>;
 };
