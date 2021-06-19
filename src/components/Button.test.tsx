a7116e824fe67ce1b755a705ff72b17c618c098a
Brendan Tierney <tiernebre@gmail.com>
2021-06-19 00:02:20 -0600

    light variant test

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -21,3 +21,20 @@ it.each(ButtonColors)(
     expect(foundButton).toHaveClass(`is-${color}`);
   }
 );
+
+it.each<ButtonColor>(["primary", "danger", "warning", "success"])(
+  "displays the button in a light variant with color = %p",
+  (color: ButtonColor) => {
+    const text = `Button ${color} Light Test`;
+    render(
+      <Button color={color} light>
+        {text}
+      </Button>
+    );
+    const foundButton = screen.getByRole("button", {
+      name: text,
+    });
+    expect(foundButton).toHaveClass(`is-${color}`);
+    expect(foundButton).toHaveClass("is-light");
+  }
+);
