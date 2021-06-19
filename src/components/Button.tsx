df20bdb34eebd5aa6501c9031c47ace69cccefb1
Brendan Tierney <tiernebre@gmail.com>
2021-06-18 23:07:12 -0600

    button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,17 @@
-export const Button = (): JSX.Element => {
-  return <button>Hi, it's Kecleon! An update happened, sweeeeeet</button>;
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonColor = "white" | "light" | "dark" | "black" | "text" | "ghost";
+
+export type ButtonProps = {
+  color?: ButtonColor;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = (props: ButtonProps): JSX.Element => {
+  let className = "button";
+
+  if (props.color) {
+    className += `is-${props.color.toLowerCase()}`;
+  }
+
+  return <button className={className}>{props.children}</button>;
 };
