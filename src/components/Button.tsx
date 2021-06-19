e35e5d6987f8fa3585758560f32c174ce6bb33cb
Brendan Tierney <tiernebre@gmail.com>
2021-06-18 23:25:05 -0600

    more tests!

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,6 +7,12 @@ export const ButtonColors = [
   "black",
   "text",
   "ghost",
+  "primary",
+  "link",
+  "info",
+  "success",
+  "warning",
+  "danger",
 ] as const;
 
 export type ButtonColor = typeof ButtonColors[number];
