92de7fd521873643c20521c0f51be5cda2f126a9
Brendan Tierney <tiernebre@gmail.com>
2021-06-19 23:04:28 -0600

    working on click

--- src/components/icon-button/IconButton.test.tsx
+++ src/components/icon-button/IconButton.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { ButtonProps } from "../button";
 import { IconButton } from "./IconButton";
+import user from "@testing-library/user-event";
 
 it("renders given children content", () => {
   const message = "Icon Button Text";
@@ -10,4 +12,20 @@ it("renders given children content", () => {
   );
   const foundIconButton = screen.getByRole("button", { name: message });
   expect(foundIconButton).toBeInTheDocument();
+});
+
+it("renders the button with the correct provided details", () => {
+  const message = "Icon Button Text";
+  const onClick = jest.fn();
+  const button: ButtonProps = {
+    onClick,
+  };
+  render(
+    <IconButton button={button} icon={{ name: "home" }}>
+      {message}
+    </IconButton>
+  );
+  const foundIconButton = screen.getByRole("button", { name: message });
+  user.click(foundIconButton);
+  expect(onClick).toHaveBeenCalled();
 });
