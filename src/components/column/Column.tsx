c04360c0bc64a25fda229c00bb5cf95a3b4486e1
Brendan Tierney <tiernebre@gmail.com>
2021-06-20 17:17:52 -0600

    column numbers

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -11,6 +11,18 @@ export const columnSizes = [
   "three-fifths",
   "two-fifths",
   "one-fifth",
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
 ] as const;
 
 export type ColumnSize = typeof columnSizes[number];
