f37ed78ddb90652351c63f1cd5cf8274a61ba2ab
Brendan Tierney <tiernebre@gmail.com>
2021-06-19 21:07:10 -0600

    simple home example

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,10 +1,7 @@
 export const Icon = (): JSX.Element => {
   return (
-    <span className="icon-text">
-      <span className="icon has-text-success">
-        <i className="fas fa-home"></i>
-      </span>
-      <span>Home</span>
+    <span className="icon">
+      <i className="fas fa-home"></i>
     </span>
   );
 };
