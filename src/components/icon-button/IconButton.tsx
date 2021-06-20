00d3008399cb55d09081aaabdcb60e518c1bbfb8
Brendan Tierney <tiernebre@gmail.com>
2021-06-19 22:22:48 -0600

    icon button setup

--- src/components/icon-button/IconButton.tsx
+++ src/components/icon-button/IconButton.tsx
@@ -1,3 +1,21 @@
-export const IconButton = () => {
-  return <p>Icon Button</p>;
+import { PropsWithChildren } from "react";
+import { Button, ButtonProps } from "../button";
+import { Icon, IconProps } from "../icon";
+
+export type IconButtonProps = PropsWithChildren<{
+  icon: IconProps;
+  button: ButtonProps;
+}>;
+
+export const IconButton = ({
+  icon,
+  button,
+  children,
+}: IconButtonProps): JSX.Element => {
+  return (
+    <Button {...button}>
+      <Icon {...icon} />
+      {children}
+    </Button>
+  );
 };
