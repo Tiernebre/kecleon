b655b9cacbb5cfa91de25b02cba888b00f12fb0d
Brendan Tierney <tiernebre@gmail.com>
2021-06-20 17:49:47 -0600

    mobile mode tests

--- src/components/columns/Columns.test.tsx
+++ src/components/columns/Columns.test.tsx
@@ -12,3 +12,36 @@ it("renders given children", () => {
   const columnFound = screen.getByText(message);
   expect(columnFound).toBeInTheDocument();
 });
+
+it("renders in mobile mode if provided", () => {
+  const message = "Hello, I'm a column!";
+  render(
+    <Columns mobile>
+      <Column>{message}</Column>
+    </Columns>
+  );
+  const columnFound = screen.getByText(message);
+  expect(columnFound.parentElement).toHaveClass("is-mobile");
+});
+
+it("renders in mobile mode if provided as false", () => {
+  const message = "Hello, I'm a column!";
+  render(
+    <Columns mobile={false}>
+      <Column>{message}</Column>
+    </Columns>
+  );
+  const columnFound = screen.getByText(message);
+  expect(columnFound.parentElement).not.toHaveClass("is-mobile");
+});
+
+it("renders in non mobile mode by default", () => {
+  const message = "Hello, I'm a column!";
+  render(
+    <Columns>
+      <Column>{message}</Column>
+    </Columns>
+  );
+  const columnFound = screen.getByText(message);
+  expect(columnFound.parentElement).not.toHaveClass("is-mobile");
+});
