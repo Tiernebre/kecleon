5d4a6d6d2858ad7ce79f1cceb431c819e91af925
Brendan Tierney <tiernebre@gmail.com>
2021-06-19 21:57:55 -0600

    screen reader only message

--- src/components/icon/Icon.test.tsx
+++ src/components/icon/Icon.test.tsx
@@ -7,3 +7,10 @@ it("renders an icon", () => {
   const icon = screen.getByTitle(message);
   expect(icon).toBeInTheDocument();
 });
+
+it("renders the message for screen readers only", () => {
+  const message = "Create Test";
+  render(<Icon name="plus" message={message} />);
+  const screenOnlyMessage = screen.getByText(message);
+  expect(screenOnlyMessage).toHaveClass("is-sr-only");
+});
