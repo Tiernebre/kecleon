36aad640f18728091ee53cf0ad43ef448611735d
Brendan Tierney <tiernebre@gmail.com>
2021-06-20 18:09:35 -0600

    container props

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,3 +1,17 @@
-export const Container = (): JSX.Element => (
-  <div className="container">Container</div>
-);
+import { PropsWithChildren } from "react";
+
+export type ContainerProps = PropsWithChildren<{
+  fluid?: boolean;
+}>;
+
+export const Container = ({ fluid, children }: ContainerProps): JSX.Element => {
+  const classes = ["container"];
+
+  if (fluid) {
+    classes.push("is-fluid");
+  }
+
+  const className = classes.join(" ");
+
+  return <div className={className}>{children}</div>;
+};
