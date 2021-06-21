e8bf9c7885a7292e1422f9d7c11f842d999c40e3
Brendan Tierney <tiernebre@gmail.com>
2021-06-20 18:23:05 -0600

    empty string for no arguments

--- src/utilities/create-class-name/create-class-name.test.ts
+++ src/utilities/create-class-name/create-class-name.test.ts
@@ -1,5 +1,9 @@
 import { createClassName } from "./create-class-name";
 
+it("returns an empty string if nothing is provided", () => {
+  expect(createClassName()).toEqual("");
+});
+
 it("returns given default class names", () => {
   const defaultClasses = ["column", "is-mobile"];
   const className = createClassName(defaultClasses);
