5cc55aa33f52b1e89e94ebb14fea52080c66c5df
Brendan Tierney <tiernebre@gmail.com>
2021-06-21 21:25:56 -0600

    normal button props

--- src/components/delete/Delete.test.tsx
+++ src/components/delete/Delete.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Size, sizes } from "../../types";
 import { Delete } from "./Delete";
+import user from "@testing-library/user-event";
 
 it.each<Size>(sizes)("renders in size %p when provided", (size: Size) => {
   render(<Delete size={size} label="Delete Test" />);
@@ -13,4 +14,14 @@ it("binds an accessible label for screen readers", () => {
   render(<Delete label={label} />);
   const foundDelete = screen.getByRole("button", { name: label });
   expect(foundDelete).toHaveAccessibleName(label);
+});
+
+it("supports normal button props", () => {
+  const onClick = jest.fn();
+  const type = "submit";
+  render(<Delete label="Label" onClick={onClick} type={type} />);
+  const foundDelete = screen.getByRole("button");
+  user.click(foundDelete);
+  expect(onClick).toHaveBeenCalled();
+  expect(foundDelete).toHaveAttribute("type", type);
 });
