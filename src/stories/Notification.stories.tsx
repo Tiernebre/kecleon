bc7cfb4f46963a3468d8dee1121c2c77544a040b
Brendan Tierney <tiernebre@gmail.com>
2021-06-21 21:35:09 -0600

    notification content

--- src/stories/Notification.stories.tsx
+++ src/stories/Notification.stories.tsx
@@ -1,13 +1,42 @@
 import { Story, Meta } from "@storybook/react";
 
 import { Notification, NotificationProps } from "../components";
+import { colors } from "../types";
 
 export default {
   title: "Example/Notification",
   component: Notification,
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+        options: colors,
+      },
+    },
+    light: {
+      control: {
+        type: "boolean",
+      },
+    },
+    closable: {
+      control: {
+        type: "boolean",
+      },
+    },
+    children: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    children: "This is a test notification!",
+  },
 } as Meta<NotificationProps>;
 
-const Template: Story<NotificationProps> = (args) => <Notification {...args} />;
+const InteractiveNotificationTemplate: Story<NotificationProps> = (args) => (
+  <Notification {...args} />
+);
 
-export const DefaultNotification = Template.bind({});
-DefaultNotification.args = {};
+export const InteractiveNotification = InteractiveNotificationTemplate.bind({});
+InteractiveNotification.args = {};
