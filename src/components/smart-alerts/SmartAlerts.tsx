66fc1789b412144220eb7aaf37ea0deaea6c4614
Brendan Tierney <tiernebre@gmail.com>
2021-06-23 22:21:35 -0600

    smart alerts setup

--- src/components/smart-alerts/SmartAlerts.tsx
+++ src/components/smart-alerts/SmartAlerts.tsx
@@ -1,3 +1,13 @@
+/**
+ * SmartAlerts is a flavor of the {@link Alerts} component
+ * that is "smart" in that it knows about Alerts context state.
+ *
+ * Any consumer of SmartAlerts will need to wrap it with an {@link AlertsProvider}
+ * context component in order for the alerts to work properly.
+ *
+ * If you do not wish to use the "smart" stateful coupled nature
+ * of this component, you should prefer to just use {@link Alerts} instead.
+ */
 export const SmartAlerts = (): JSX.Element => {
   return <div>Smart Alerts</div>;
 };
