39912c4bf6b6efa32c396e004e88f6400490ccfd
Brendan Tierney <tiernebre@gmail.com>
2021-06-23 21:20:09 -0600

    typing ftw

---
+++ src/hooks/alerts/AlertsContext.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { AlertRequest } from "../../types/alert";
+
+type Action = { type: "add"; payload: AlertRequest };
+type Dispatch = (action: Action) => void;
+type State = {
+  alerts: AlertRequest[];
+};
+type AlertsProviderProps = React.PropsWithChildren<unknown>;
+type Context = { state: State; dispatch: Dispatch };
+
+const AlertsContext = React.createContext<Context | undefined>(undefined);
+
+const alertsReducer: React.Reducer<State, Action> = (
+  state: State,
+  action: Action
+) => {
+  switch (action.type) {
+    case "add":
+      return { alerts: [...state.alerts, action.payload] };
+    default:
+      throw new Error(`Unhandled alerts action type`);
+  }
+};
+
+export const AlertsProvider = ({
+  children,
+}: AlertsProviderProps): JSX.Element => {
+  const [state, dispatch] = React.useReducer(alertsReducer, { alerts: [] });
+  const value = { state, dispatch };
+
+  return (
+    <AlertsContext.Provider value={value}>{children}</AlertsContext.Provider>
+  );
+};
+
+export const useAlerts = (): Context => {
+  const context = React.useContext(AlertsContext);
+  if (context === undefined) {
+    throw new Error("useAlerts must be used within an AlertsProvider");
+  }
+  return context;
+};
