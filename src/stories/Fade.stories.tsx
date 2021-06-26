04eb0dd7e6d1b350e73d654a969b6a7ff6030cd5
Brendan Tierney <tiernebre@gmail.com>
2021-06-25 22:17:00 -0600

    fully invisible when done

--- src/stories/Fade.stories.tsx
+++ src/stories/Fade.stories.tsx
@@ -23,7 +23,10 @@ export default {
     },
   },
   args: {
-    children: "This message will fade away shortly...",
+    visible: true,
+    children:
+      "This message will fade in and out depending on visibility. Try interacting with the control knob for visible below to see!",
+    durationInMillis: 5000,
   },
 } as Meta<FadeProps>;
 
