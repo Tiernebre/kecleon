1d36305e141d32b0709399b87cd1d481bb866e47
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 11:27:06 -0600

    image class name

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,13 +1,26 @@
 import { ImgHTMLAttributes } from "react";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../utilities";
 
 export type ImageProps = ImgHTMLAttributes<HTMLImageElement> & {
   rounded?: boolean;
   alt: string;
 };
 
+const classNameMapping: ClassNameTransformMap<ImageProps> = new Map([
+  ["rounded", () => "is-rounded"],
+]);
+
 export const Image = ({ rounded, alt, ...props }: ImageProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMapping,
+    { rounded } as Partial<ImageProps>,
+    ["image"]
+  );
   return (
-    <figure>
+    <figure className={className}>
       <img alt={alt} {...props} />
     </figure>
   );
