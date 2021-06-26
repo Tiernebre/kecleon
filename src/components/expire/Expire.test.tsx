8d6085070b2ac2f7e7278520839917c1907bcda8
Brendan Tierney <tiernebre@gmail.com>
2021-06-25 19:00:38 -0600

    test expire

---
+++ src/components/expire/Expire.test.tsx
@@ -0,0 +1,14 @@
+import { act, render, screen } from "@testing-library/react";
+import { Expire } from "./Expire";
+
+it("removes children after a set amount of time", () => {
+  jest.useFakeTimers();
+  const message = "Message!";
+  const time = 10000;
+  render(<Expire time={time}>{message}</Expire>);
+  expect(screen.getByText(message)).toBeInTheDocument();
+  act(() => {
+    jest.runAllTimers();
+  });
+  expect(screen.queryByText(message)).toBeNull();
+});
--- src/components/expire/Expire.tsx
+++ src/components/expire/Expire.tsx
@@ -1,3 +1,21 @@
-export const Expire = (): JSX.Element => {
-  return <div>Expire</div>;
+import { Fragment, PropsWithChildren, useEffect, useState } from "react";
+
+export type ExpireProps = PropsWithChildren<{
+  time: number;
+}>;
+
+export const Expire = ({ time, children }: ExpireProps): JSX.Element => {
+  const [expired, setExpired] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setExpired(true);
+    }, time);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [time]);
+
+  return <Fragment>{!expired && children}</Fragment>;
 };
