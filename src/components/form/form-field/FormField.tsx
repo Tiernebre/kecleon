b0c964de85638764be7c6c544043de5081d750c6
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 17:56:48 -0600

    form field test

--- src/components/form/form-field/FormField.tsx
+++ src/components/form/form-field/FormField.tsx
@@ -1,3 +1,7 @@
-export const FormField = (): JSX.Element => {
-  return <div className="field">Form Field</div>;
+import { PropsWithChildren } from "react";
+
+export const FormField = ({
+  children,
+}: PropsWithChildren<unknown>): JSX.Element => {
+  return <div className="field">{children}</div>;
 };
