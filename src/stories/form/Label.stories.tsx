7cf6d0a280803e3ed582b735e6dd2bf144626b81
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 18:56:59 -0600

    form field label

--- src/stories/form/Label.stories.tsx
+++ src/stories/form/Label.stories.tsx
@@ -4,11 +4,17 @@ import { Label } from "../../components";
 export default {
   component: Label,
   title: "Form/Label",
+  argTypes: {
+    children: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    children: "Form Field Label",
+  },
 } as Meta;
 
-const Template: Story = () => (
-  <form>
-    <Label>Form Field Label</Label>
-  </form>
-);
+const Template: Story = (args) => <Label {...args} />;
 export const StandardFormLabel = Template.bind({});
