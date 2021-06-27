fd0da8b464a65fe90e55b6bc8bddc1052b9489bc
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 18:21:37 -0600

    textarea props

--- src/components/form/textarea/Textarea.tsx
+++ src/components/form/textarea/Textarea.tsx
@@ -1,3 +1,7 @@
-export const Textarea = (): JSX.Element => {
-  return <div>Textarea</div>;
+import { TextareaHTMLAttributes } from "react";
+
+export type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+export const Textarea = (props: TextareaProps): JSX.Element => {
+  return <textarea className="textarea" {...props} />;
 };
