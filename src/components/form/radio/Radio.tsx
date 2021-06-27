77a6c8efeb0f9f05e9e9d4e3fcffdcd94b8243c4
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 23:04:39 -0600

    radio tests

--- src/components/form/radio/Radio.tsx
+++ src/components/form/radio/Radio.tsx
@@ -2,10 +2,10 @@ import { InputHTMLAttributes } from "react";
 
 export type RadioProps = Omit<InputHTMLAttributes<HTMLInputElement>, "type">;
 
-export const Radio = ({ children, ...props }: RadioProps): JSX.Element => {
+export const Radio = ({ children, id, ...props }: RadioProps): JSX.Element => {
   return (
-    <label className="radio">
-      <input {...props} type="radio" />
+    <label htmlFor={id} className="radio">
+      <input id={id} {...props} type="radio" />
       {children}
     </label>
   );
