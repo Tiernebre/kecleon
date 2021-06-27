30d37764fbe43c459d4d1f1a3b42819ad5962858
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 22:48:05 -0600

    feat: Implement Checkbox (#49)
    * props test
    
    * checkbox export
    
    * test id prop
    
    * checkbox on change test
    
    * select option test
    
    * checkbox setup

---
+++ src/components/form/checkbox/Checkbox.tsx
@@ -0,0 +1,18 @@
+import { InputHTMLAttributes } from "react";
+
+export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
+  id?: string;
+};
+
+export const Checkbox = ({
+  id,
+  children,
+  ...props
+}: CheckboxProps): JSX.Element => {
+  return (
+    <label htmlFor={id} className="checkbox">
+      <input {...props} id={id} type="checkbox" />
+      {children}
+    </label>
+  );
+};
