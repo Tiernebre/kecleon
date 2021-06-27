dbf03306411aac2a22b394b2a8d9a3b141519f81
Brendan Tierney <tiernebre@gmail.com>
2021-06-26 22:37:04 -0600

    checkbox on change test

--- src/components/form/checkbox/Checkbox.test.tsx
+++ src/components/form/checkbox/Checkbox.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Checkbox } from "./Checkbox";
+import user from "@testing-library/user-event";
 
 it("ties the given ID to the checkbox input", () => {
   const label = "Test Checkbox";
@@ -7,4 +8,21 @@ it("ties the given ID to the checkbox input", () => {
   render(<Checkbox id={id}>{label}</Checkbox>);
   const checkbox = screen.getByLabelText(label);
   expect(checkbox).toBeInTheDocument();
+});
+
+it("binds to a callback when a change occurs", () => {
+  const label = "Test Checkbox";
+  const id = "test-foo-baz";
+  const onChange = jest.fn();
+  render(
+    <Checkbox id={id} onChange={onChange}>
+      {label}
+    </Checkbox>
+  );
+  expect(onChange).not.toHaveBeenCalled();
+  const checkbox = screen.getByLabelText(label);
+  user.click(checkbox);
+  expect(onChange).toHaveBeenCalledTimes(1);
+  user.click(checkbox);
+  expect(onChange).toHaveBeenCalledTimes(2);
 });
