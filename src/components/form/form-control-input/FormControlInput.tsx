05feafd1ccc97620c0507f887007d7a9fe47db71
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 20:59:31 -0600

    smart form control input

--- src/components/form/form-control-input/FormControlInput.tsx
+++ src/components/form/form-control-input/FormControlInput.tsx
@@ -1,9 +1,23 @@
+import { InputProps, SelectProps } from "..";
 import { FormControlInputType } from "../../../types";
+import { Button, ButtonProps } from "../../button";
+import { IconProps } from "../../icon";
 
-export type FormControlInputProps = {
-  input: FormControlInputType;
-};
+export type FormControlInputProps = InputProps &
+  SelectProps &
+  ButtonProps &
+  IconProps & {
+    input: FormControlInputType;
+  };
 
-export const FormControlInput = (): JSX.Element => {
-  return <div>FormControlInput</div>;
+export const FormControlInput = ({
+  input,
+  ...props
+}: FormControlInputProps): JSX.Element | null => {
+  switch (input) {
+    case "button":
+      return <Button {...props} />;
+    default:
+      return null;
+  }
 };
