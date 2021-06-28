ee58c72620fd5b6bfb3895bce0e6df4df960682e
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 20:03:14 -0600

    empty array test

--- src/components/form/mapped-select/MappedSelect.test.tsx
+++ src/components/form/mapped-select/MappedSelect.test.tsx
@@ -9,7 +9,12 @@ type TestData = {
   active: boolean;
 };
 
-it("renders a data structure into a select with corresponding options", () => {
+it("renders no options if an empty array is provided", () => {
+  render(<MappedSelect options={[]} mapToOption={jest.fn()} />);
+  expect(screen.queryAllByRole("option")).toHaveLength(0);
+});
+
+it("renders a list of data structures into a select with corresponding options", () => {
   const options: TestData[] = [
     {
       id: 1,
@@ -29,7 +34,7 @@ it("renders a data structure into a select with corresponding options", () => {
     value: testData.id,
   });
   render(<MappedSelect options={options} mapToOption={mapToOption} />);
-  expect(screen.queryAllByRole("option")).toHaveLength(2);
+  expect(screen.getAllByRole("option")).toHaveLength(2);
   options.forEach((option) => {
     const optionFound = screen.getByRole("option", { name: option.name });
     expect(optionFound).toBeInTheDocument();
