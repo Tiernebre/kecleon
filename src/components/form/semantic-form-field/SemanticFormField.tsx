5d0fad960fed70dd36edd22b62e39236794d75c1
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 21:09:56 -0600

    iniital semantic form field

--- src/components/form/semantic-form-field/SemanticFormField.tsx
+++ src/components/form/semantic-form-field/SemanticFormField.tsx
@@ -1,9 +1,11 @@
-import { Label, FormControl, Input, Help } from "..";
-import { FormControlInput } from "../../../types";
+import { Label, FormControl, Help } from "..";
+import { FormControlProps } from "../form-control";
+import { FormControlInput, FormControlInputProps } from "../form-control-input";
 import { FormField } from "../form-field";
 
 type SemanticFormFieldProps = {
-  control: FormControlInput;
+  control: FormControlProps;
+  input: FormControlInputProps;
   help?: string;
   id: string;
   label: string;
@@ -13,16 +15,23 @@ type SemanticFormFieldProps = {
  * SemanticFormField is a component that combines Label, FormField,
  * and Help components to create a fully composed Form Field
  * for a typical form.
+ *
+ * Unlike these individual components, this component is based
+ * upon given prop data to then render the correct components. This
+ * component intentionally favors an inheritance approach vs
+ * composition for spinning up standard, similar inputs.
  */
 export const SemanticFormField = ({
   id,
   label,
   help,
+  control,
+  input,
 }: SemanticFormFieldProps): JSX.Element => (
   <FormField>
     <Label htmlFor={id}>{label}</Label>
-    <FormControl>
-      <Input id={id} type="text" value="Form Field Input"></Input>
+    <FormControl {...control}>
+      <FormControlInput {...input} />
     </FormControl>
     {help && <Help>help</Help>}
   </FormField>
