db938a83d2fd69711a7cc08b5e749bbe5e3bf24b
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 19:02:10 -0600

    option label test

--- src/components/form/select-options/SelectOptions.test.tsx
+++ src/components/form/select-options/SelectOptions.test.tsx
@@ -16,3 +16,22 @@ it("renders a single option if given an array with one", () => {
     screen.getByRole("option", { name: option.label })
   ).toBeInTheDocument();
 });
+
+it("renders given options", () => {
+  const optionOne = {
+    label: "Option 1",
+    value: "1",
+  };
+  const optionTwo = {
+    label: "Option 2",
+    value: "2",
+  };
+  render(<SelectOptions options={[optionOne, optionTwo]} />);
+  expect(screen.getAllByRole("option")).toHaveLength(2);
+  expect(
+    screen.getByRole("option", { name: optionOne.label })
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole("option", { name: optionTwo.label })
+  ).toBeInTheDocument();
+});
