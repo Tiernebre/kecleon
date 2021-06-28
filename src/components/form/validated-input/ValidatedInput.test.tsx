76cf931790d444c590e946991471bced3193fb04
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 21:55:07 -0600

    validated input setup

--- src/components/form/validated-input/ValidatedInput.test.tsx
+++ src/components/form/validated-input/ValidatedInput.test.tsx
@@ -10,7 +10,7 @@ it("renders in danger color if invalid", () => {
 });
 
 it.each<Color>(["success", "warning", "primary"])(
-  "overrides color prop = %p if invalid to be danger",
+  "overrides using danger if color prop = %p if and is invalid",
   (originalColor: Color) => {
     render(<ValidatedInput valid={false} color={originalColor} />);
     const input = getInput();
@@ -19,7 +19,22 @@ it.each<Color>(["success", "warning", "primary"])(
   }
 );
 
+it.each<Color>(["success", "warning", "primary"])(
+  "does not overrides color prop = %p if valid",
+  (originalColor: Color) => {
+    render(<ValidatedInput valid={true} color={originalColor} />);
+    const input = getInput();
+    expect(input).not.toHaveClass("is-danger");
+    expect(input).toHaveClass(`is-${originalColor}`);
+  }
+);
+
 it("is marked accessibly as invalid if invalid", () => {
   render(<ValidatedInput valid={false} />);
   expect(getInput()).toBeInvalid();
+});
+
+it("is marked accessibly as valid if valid", () => {
+  render(<ValidatedInput valid={true} />);
+  expect(getInput()).toBeValid();
 });
