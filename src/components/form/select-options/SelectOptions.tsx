4ddc9d8741e4f6ba398efd61bc06008fb81cfef4
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 18:55:42 -0600

    select options mapping

--- src/components/form/select-options/SelectOptions.tsx
+++ src/components/form/select-options/SelectOptions.tsx
@@ -8,10 +8,19 @@ type SelectOptionsProps = {
   options: Option[];
 };
 
+/**
+ * Anytime a `select` or `Select` is used, this component can be used
+ * for mapping an array of values into dynamically rendered `option` elements.
+ *
+ * This prevents having to do an `Array.map` everytime a Select is used and
+ * options are iterated over.
+ */
 export const SelectOptions = ({ options }: SelectOptionsProps): JSX.Element => {
   return (
     <Fragment>
-      <option></option>
+      {options.map(({ label, ...props }) => (
+        <option {...props}>{label}</option>
+      ))}
     </Fragment>
   );
 };
