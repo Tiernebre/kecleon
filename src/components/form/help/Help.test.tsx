42075b54c89adb863ecd0a8012295b310a5bc82d
Brendan Tierney <tiernebre@gmail.com>
2021-06-27 22:10:29 -0600

    help component now handles errors

--- src/components/form/help/Help.test.tsx
+++ src/components/form/help/Help.test.tsx
@@ -19,3 +19,21 @@ it("is bound to its given id", () => {
   render(<Help id={id}>{message}</Help>);
   expect(screen.getByText(message)).toHaveAttribute("id", id);
 });
+
+it("displays an error message if an error passed in is truthy", () => {
+  const id = "first-name-help";
+  const message = "This is a help text";
+  const error = {
+    type: "required",
+    message: "this field is required",
+  };
+  render(
+    <Help id={id} error={error}>
+      {message}
+    </Help>
+  );
+  const help = screen.getByText(error.message);
+  expect(help).toBeInTheDocument();
+  expect(help).toHaveClass("is-danger");
+  expect(screen.queryByText(message)).toBeNull();
+});
--- src/components/form/help/Help.tsx
+++ src/components/form/help/Help.tsx
@@ -1,11 +1,31 @@
 import { PropsWithChildren } from "react";
+import { FieldError } from "react-hook-form";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../../utilities";
 
 export type HelpProps = PropsWithChildren<{
   id?: string;
+  error?: FieldError;
 }>;
 
-export const Help = ({ id, children }: HelpProps): JSX.Element => (
-  <p id={id} className="help">
-    {children}
-  </p>
-);
+const classNameMap: ClassNameTransformMap<HelpProps> = new Map([
+  ["error", () => "is-danger"],
+]);
+
+export const Help = ({ id, error, children }: HelpProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMap,
+    { error } as Partial<HelpProps>,
+    ["help"]
+  );
+
+  const message = error?.message ? error.message : children;
+
+  return (
+    <p id={id} className={className}>
+      {message}
+    </p>
+  );
+};
