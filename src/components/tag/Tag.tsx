a30b755b61fac0a1c21702a1b3fb694eeec544e4
Brendan Tierney <tiernebre@gmail.com>
2021-06-30 19:11:30 -0600

    tag color prop setup

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,1 +1,24 @@
-export const Tag = (): JSX.Element => <div>Tag</div>;
+import { PropsWithChildren } from "react";
+import { Color } from "../../types";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+  mapClassNameForColor,
+} from "../../utilities";
+
+export type TagProps = PropsWithChildren<{
+  color?: Color;
+}>;
+
+const classNameMapping: ClassNameTransformMap<TagProps> = new Map([
+  mapClassNameForColor(),
+]);
+
+export const Tag = ({ color, children }: TagProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMapping,
+    { color } as Partial<TagProps>,
+    ["tag"]
+  );
+  return <span className={className}>{children}</span>;
+};
