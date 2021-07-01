f3b49b57d80728fdc71dbe86131cd7b7dda16eef
Brendan Tierney <tiernebre@gmail.com>
2021-06-30 19:22:48 -0600

    initial test

--- src/components/tags/Tags.test.tsx
+++ src/components/tags/Tags.test.tsx
@@ -1,12 +1,16 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Tag } from "../tag/Tag";
 import { Tags } from "./Tags";
 
 it("renders given children", () => {
+  const firstTag = "Test Tag 1";
+  const secondTag = "Test Tag 2";
   render(
     <Tags>
-      <Tag>Test Tag 1</Tag>
-      <Tag>Test Tag 2</Tag>
+      <Tag>{firstTag}</Tag>
+      <Tag>{secondTag}</Tag>
     </Tags>
   );
+  expect(screen.getByText(firstTag)).toBeInTheDocument();
+  expect(screen.getByText(secondTag)).toBeInTheDocument();
 });
