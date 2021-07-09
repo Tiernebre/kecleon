dec8f58437e797487f4d2d7caccccedc390a024d
Brendan Tierney <tiernebre@gmail.com>
2021-07-08 18:05:00 -0600

    borderable tests

--- src/components/table/Table.test.tsx
+++ src/components/table/Table.test.tsx
@@ -5,3 +5,13 @@ it("is rendered with unopinionated styling by default", () => {
   render(<Table />);
   expect(screen.getByRole("table")).toHaveClass("table", { exact: true });
 });
+
+it("can be rendered with borders", () => {
+  render(<Table bordered />);
+  expect(screen.getByRole("table")).toHaveClass("is-bordered");
+});
+
+it("can be rendered without borders", () => {
+  render(<Table bordered={false} />);
+  expect(screen.getByRole("table")).not.toHaveClass("is-bordered");
+});
