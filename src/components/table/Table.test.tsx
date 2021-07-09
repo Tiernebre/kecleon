545330eba5ccc652a60f6a564808bc6b81a25cbb
Brendan Tierney <tiernebre@gmail.com>
2021-07-08 18:10:06 -0600

    full width test

--- src/components/table/Table.test.tsx
+++ src/components/table/Table.test.tsx
@@ -37,3 +37,13 @@ it("can be rendered without hoverable focus", () => {
   render(<Table hoverable={false} />);
   expect(getTable()).not.toHaveClass("is-hoverable");
 });
+
+it("can be rendered in full width", () => {
+  render(<Table fullwidth />);
+  expect(getTable()).toHaveClass("is-fullwidth");
+});
+
+it("can be rendered in non full width", () => {
+  render(<Table fullwidth={false} />);
+  expect(getTable()).not.toHaveClass("is-fullwidth");
+});
