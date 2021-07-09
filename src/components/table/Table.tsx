9e654403738d561ae106d1e9cb71514e7d89666d
Brendan Tierney <tiernebre@gmail.com>
2021-07-08 18:04:09 -0600

    initial table prop

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,3 +1,37 @@
-export const Table = (): JSX.Element => {
-  return <table className="table">Table</table>;
+import { TableHTMLAttributes } from "react";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../utilities";
+
+export type TableProps = TableHTMLAttributes<HTMLTableElement> & {
+  bordered?: boolean;
+  striped?: boolean;
+  narrow?: boolean;
+  hoverable?: boolean;
+  fullwidth?: boolean;
+};
+
+const classNameMapping: ClassNameTransformMap<TableProps> = new Map([
+  ["bordered", () => "is-bordered"],
+  ["striped", () => "is-striped"],
+  ["narrow", () => "is-narrow"],
+  ["hoverable", () => "is-hoverable"],
+  ["fullwidth", () => "is-fullwidth"],
+]);
+
+export const Table = ({
+  bordered,
+  striped,
+  narrow,
+  hoverable,
+  fullwidth,
+  ...props
+}: TableProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMapping,
+    { bordered, striped, narrow, hoverable, fullwidth } as Partial<TableProps>,
+    ["table"]
+  );
+  return <table className={className}>Table</table>;
 };
