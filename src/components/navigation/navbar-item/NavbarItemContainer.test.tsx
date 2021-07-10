7f4aaf35b1263789fa05252cc653ab7158a6caab
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 13:07:13 -0600

    renders test

--- src/components/navigation/navbar-item/NavbarItemContainer.test.tsx
+++ src/components/navigation/navbar-item/NavbarItemContainer.test.tsx
@@ -1,6 +1,12 @@
 import { screen, render } from "@testing-library/react";
 import { NavbarItemContainer } from "./NavbarItemContainer";
 
+it("renders given children", () => {
+  const content = "Navbar Item Container";
+  render(<NavbarItemContainer>{content}</NavbarItemContainer>);
+  expect(screen.getByText(content)).toBeInTheDocument();
+});
+
 it("is rendered with unopinionated styling", () => {
   const content = "Navbar Item Container";
   render(<NavbarItemContainer>{content}</NavbarItemContainer>);
