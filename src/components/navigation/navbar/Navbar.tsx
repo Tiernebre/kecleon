68d3c5ddc169c8f488a1f47a3df126b9689b6a20
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 00:30:39 -0600

    fixed

--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import { Position } from "../../../types";
 import {
   ClassNameTransformMap,
   createClassNameFromProps,
@@ -6,12 +7,23 @@ import {
 
 export type NavbarProps = PropsWithChildren<{
   transparent?: boolean;
+  fixed?: Position;
 }>;
 
 const classNameMapping: ClassNameTransformMap<NavbarProps> = new Map([
   ["transparent", () => "is-transparent"],
+  ["fixed", (position: string) => `is-fixed-${position}`],
 ]);
 
+/**
+ * Navbar is a component for building a navigation bar.
+ *
+ * Please note: if you are using the "fixed" prop, you will need to add
+ * the respective class to either <html> or <body> for anchoring the fixed
+ * navigation bar correctly. These classes are **has-navbar-fixed-top** or
+ * **has-navbar-fixed-bottom** dependent upon your position. Eventually
+ * I may automate this to be added a class to the HTML/Body... stay tuned.
+ */
 export const Navbar = ({ transparent, children }: NavbarProps): JSX.Element => {
   const className = createClassNameFromProps(
     classNameMapping,
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ export * from "./alert";
 export * from "./color";
 export * from "./direction";
 export * from "./form";
+export * from "./position";
 export * from "./size";
 export * from "./typography";
