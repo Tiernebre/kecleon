bc19b6c95105050e8ae8086ba9551065f6884187
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 12:44:28 -0600

    clean up docs

--- src/components/navigation/navbar-item/NavbarItemLink.tsx
+++ src/components/navigation/navbar-item/NavbarItemLink.tsx
@@ -2,17 +2,6 @@ import { Anchor, AnchorProps } from "../../anchor";
 
 export type NavbarItemLinkProps = AnchorProps;
 
-/**
- * A navigation item link that supports the ability to be a
- * single-page application link or a traditional server-side
- * anchor element.
- *
- * If you wish to use a single page router link, you will need to provide the
- * `to` attribute.
- *
- * If you wish to use the traditional server-side anchor tag, you will need
- * to provide the `href` attribute.
- */
 export const NavbarItemLink = (
   props: NavbarItemLinkProps
 ): JSX.Element | null => {
