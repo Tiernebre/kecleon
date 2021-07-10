35490eebf5422644fc59aaa339cfddf899b402e3
Brendan Tierney <tiernebre@gmail.com>
2021-07-09 20:10:57 -0600

    heading group test

--- src/components/typography/heading-group/HeadingGroup.test.tsx
+++ src/components/typography/heading-group/HeadingGroup.test.tsx
@@ -8,3 +8,24 @@ it("renders with a default level of 3 for the title", () => {
   expect(foundTitle).toHaveClass("is-3");
   expect(foundTitle.nodeName).toEqual("H3");
 });
+
+it("renders a given subtitle", () => {
+  const title = "Title";
+  const subtitle = "subtitle";
+  render(<HeadingGroup title={title} subtitle={subtitle} />);
+  const foundTitle = screen.getByText(title);
+  expect(foundTitle).toBeInTheDocument();
+  const foundSubtitle = screen.getByText(subtitle);
+  expect(foundSubtitle).toBeInTheDocument();
+});
+
+it("renders a subtitle with level of 5 by default", () => {
+  const title = "Title";
+  const subtitle = "subtitle";
+  render(<HeadingGroup title={title} subtitle={subtitle} />);
+  const foundTitle = screen.getByText(title);
+  expect(foundTitle).toBeInTheDocument();
+  const foundSubtitle = screen.getByText(subtitle);
+  expect(foundSubtitle).toHaveClass("is-5");
+  expect(foundSubtitle.nodeName).toEqual("H5");
+});
