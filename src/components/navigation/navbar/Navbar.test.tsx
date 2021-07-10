470c7176778dfabdf266f196612c80b74da20c8f
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 00:25:57 -0600

    transparent tests

--- src/components/navigation/navbar/Navbar.test.tsx
+++ src/components/navigation/navbar/Navbar.test.tsx
@@ -7,3 +7,13 @@ it("by default has unopinionated styling", () => {
   render(<Navbar />);
   expect(getNavbar()).toHaveClass("navbar", { exact: true });
 });
+
+it("can be rendered in transparent color", () => {
+  render(<Navbar transparent />);
+  expect(getNavbar()).toHaveClass("is-transparent");
+});
+
+it("can be rendered in non-transparent color", () => {
+  render(<Navbar transparent={false} />);
+  expect(getNavbar()).not.toHaveClass("is-transparent");
+});
--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -1,3 +1,22 @@
-export const Navbar = (): JSX.Element => {
-  return <nav className="navbar">Navbar</nav>;
+import { PropsWithChildren } from "react";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../../utilities";
+
+export type NavbarProps = PropsWithChildren<{
+  transparent?: boolean;
+}>;
+
+const classNameMapping: ClassNameTransformMap<NavbarProps> = new Map([
+  ["transparent", () => "is-transparent"],
+]);
+
+export const Navbar = ({ transparent, children }: NavbarProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMapping,
+    { transparent } as Partial<NavbarProps>,
+    ["navbar"]
+  );
+  return <nav className={className}>{children}</nav>;
 };
