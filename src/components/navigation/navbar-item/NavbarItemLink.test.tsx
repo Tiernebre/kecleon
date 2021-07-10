bc5bd0348c18d2a2e9ebc0f8fa8a1d55429baeda
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 12:17:49 -0600

    class name tests

--- src/components/navigation/navbar-item/NavbarItemLink.test.tsx
+++ src/components/navigation/navbar-item/NavbarItemLink.test.tsx
@@ -2,25 +2,23 @@ import { screen, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { NavbarItemLink } from "./NavbarItemLink";
 
-it("is rendered without opinionated styling by default", () => {
-  const text = "NavbarItemLink";
+const getLink = () => screen.getByRole("link");
+
+it("is rendered without opinionated styling by default for single page routing", () => {
   render(
     <MemoryRouter>
-      <NavbarItemLink to="some-custom-path">{text}</NavbarItemLink>
+      <NavbarItemLink to="some-custom-path">Link</NavbarItemLink>
     </MemoryRouter>
   );
-  const renderedLink = screen.getByText(text);
-  expect(renderedLink).toHaveClass("navbar-item", { exact: true });
+  expect(getLink()).toHaveClass("navbar-item", { exact: true });
 });
 
-it("is rendered as a traditional anchor element tag if href is provided", () => {
-  const text = "NavbarItemLink";
+it("is rendered without opionated styling by default for server side routing", () => {
   const href = "https://www.google.com";
   render(
     <MemoryRouter>
-      <NavbarItemLink href={href}>{text}</NavbarItemLink>
+      <NavbarItemLink href={href}>Link</NavbarItemLink>
     </MemoryRouter>
   );
-  const renderedLink = screen.getByText(text);
-  expect(renderedLink).toHaveClass("navbar-item", { exact: true });
+  expect(getLink()).toHaveClass("navbar-item", { exact: true });
 });
