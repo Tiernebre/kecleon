edc8f8869eec459727f64449a2992d0d125935da
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 12:15:56 -0600

    docs

--- src/components/navigation/navbar-item/NavbarItemLink.tsx
+++ src/components/navigation/navbar-item/NavbarItemLink.tsx
@@ -3,6 +3,17 @@ import { PartialBy } from "../../..";
 
 export type NavbarItemLinkProps = PartialBy<LinkProps, "to">;
 
+/**
+ * A navigation item link that supports the ability to be a
+ * single-page application link or a traditional server-side
+ * anchor element.
+ *
+ * If you wish to use a single page router link, you will need to provide the
+ * `to` attribute.
+ *
+ * If you wish to use the traditional server-side anchor tag, you will need
+ * to provide the `href` attribute.
+ */
 export const NavbarItemLink = ({
   children,
   to,
