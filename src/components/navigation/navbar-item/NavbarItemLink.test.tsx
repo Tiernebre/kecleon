44c0c67bf3dfea41f03134865ffc4269cd60f008
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 12:37:49 -0600

    working SPA route test

--- src/components/navigation/navbar-item/NavbarItemLink.test.tsx
+++ src/components/navigation/navbar-item/NavbarItemLink.test.tsx
@@ -1,6 +1,8 @@
-import { screen, render } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { screen, render, waitFor } from "@testing-library/react";
+import { MemoryRouter, Switch } from "react-router";
+import { Route } from "react-router-dom";
 import { NavbarItemLink } from "./NavbarItemLink";
+import user from "@testing-library/user-event";
 
 const getLink = () => screen.getByRole("link");
 
@@ -17,4 +19,26 @@ it("is rendered without opionated styling by default for server side routing", (
   const href = "https://www.google.com";
   render(<NavbarItemLink href={href}>Link</NavbarItemLink>);
   expect(getLink()).toHaveClass("navbar-item", { exact: true });
+});
+
+it("will single page route to an existing route in the page", async () => {
+  const expectedRoutePath = "/test-route";
+  const expectedRouteContent = "Expected Route";
+  const unexpectedRouteContent = "Unexpected Route";
+  render(
+    <MemoryRouter>
+      <NavbarItemLink to={expectedRoutePath}>Link</NavbarItemLink>
+      <Switch>
+        <Route exact path="/">
+          Default
+        </Route>
+        <Route path={expectedRoutePath}>{expectedRouteContent}</Route>
+        <Route path="/some-other-route">{unexpectedRouteContent}</Route>
+      </Switch>
+    </MemoryRouter>
+  );
+  user.click(getLink());
+  await screen.findByText(expectedRouteContent);
+  expect(screen.getByText(expectedRouteContent)).toBeInTheDocument();
+  expect(screen.queryByText(unexpectedRouteContent)).toBeNull();
 });
--- src/stories/navigation/NavbarItemLink.stories.tsx
+++ src/stories/navigation/NavbarItemLink.stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from "@storybook/react";
-import { MemoryRouter } from "react-router";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import {
   NavbarItemLink,
@@ -28,9 +28,38 @@ export default {
 } as Meta<NavbarItemLinkProps>;
 
 const Template: Story<NavbarItemLinkProps> = (args) => (
-  <MemoryRouter>
-    <NavbarItemLink {...args} />
-  </MemoryRouter>
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <NavbarItemLink to="/about">About</NavbarItemLink>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+      <Switch>
+        <Route exact path="/">
+          Home
+        </Route>
+        <Route path="/about">About</Route>
+        <Route path="/dashboard">Dashboard</Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export const InteractiveNavbarItemLink = Template.bind({});
