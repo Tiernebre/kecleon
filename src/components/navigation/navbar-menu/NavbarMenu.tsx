17c6f75c4b6c5151fb0abb2f84ff2b84963a9358
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 23:11:15 -0600

    navbar menu

--- src/components/navigation/navbar-menu/NavbarMenu.tsx
+++ src/components/navigation/navbar-menu/NavbarMenu.tsx
@@ -1,3 +1,35 @@
-export const NavbarMenu = (): JSX.Element => {
-  return <div>Navbar Menu</div>;
+import { AbstractView, PropsWithChildren, ReactNode } from "react";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../../utilities";
+
+export type NavbarMenuProps = PropsWithChildren<{
+  start?: ReactNode;
+  end?: ReactNode;
+  active?: AbstractView;
+}>;
+
+const classNameMap: ClassNameTransformMap<NavbarMenuProps> = new Map([
+  ["active", () => "is-active"],
+]);
+
+export const NavbarMenu = ({
+  start,
+  end,
+  active,
+  children,
+}: NavbarMenuProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMap,
+    { active } as Partial<NavbarMenuProps>,
+    ["navbar-menu"]
+  );
+  return (
+    <div className={className}>
+      {start}
+      {children}
+      {end}
+    </div>
+  );
 };
