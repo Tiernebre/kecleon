98307082eca1c80dc4e0f30df90bc86fc57d83f8
Brendan Tierney <tiernebre@gmail.com>
2021-07-11 15:51:36 -0600

    stateful navbar

--- src/components/navigation/smart-navbar/SmartNavbar.tsx
+++ src/components/navigation/smart-navbar/SmartNavbar.tsx
@@ -1,3 +1,37 @@
-export const SmartNavbar = () => {
-  return <div>Navbar</div>;
+import { ReactNode, useState } from "react";
+import { Navbar, NavbarProps } from "../navbar";
+import { NavbarBrand } from "../navbar-brand";
+import { NavbarBurger } from "../navbar-burger";
+import { NavbarMenu } from "../navbar-menu";
+
+export type SmartNavbarProps = NavbarProps & {
+  brandItems?: ReactNode;
+  menuStartItems?: ReactNode;
+  menuEndItems?: ReactNode;
+};
+
+export const SmartNavbar = ({
+  brandItems,
+  menuStartItems,
+  menuEndItems,
+  ...props
+}: SmartNavbarProps): JSX.Element => {
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
+  const toggleMenuVisibility = () => setMenuIsVisible(!menuIsVisible);
+
+  return (
+    <Navbar {...props}>
+      <NavbarBrand
+        items={brandItems}
+        burger={
+          <NavbarBurger onClick={toggleMenuVisibility} active={menuIsVisible} />
+        }
+      />
+      <NavbarMenu
+        active={menuIsVisible}
+        start={menuStartItems}
+        end={menuEndItems}
+      />
+    </Navbar>
+  );
 };
