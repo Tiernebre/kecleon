5e7cda7be63cda4698d17ee9f41632b50a65d9cc
Brendan Tierney <tiernebre@gmail.com>
2021-07-10 21:40:02 -0600

    navbar item

--- src/components/navigation/navbar-item/NavbarItem.tsx
+++ src/components/navigation/navbar-item/NavbarItem.tsx
@@ -1,3 +1,23 @@
-export const NavbarItem = (): JSX.Element => {
-  return <div>Navbar item</div>;
+import { PropsWithChildren } from "react";
+import {
+  NavbarItemContainer,
+  NavbarItemContainerProps,
+} from "./NavbarItemContainer";
+import { NavbarItemLink, NavbarItemLinkProps } from "./NavbarItemLink";
+
+export type NavbarItemProps = PropsWithChildren<{
+  link?: NavbarItemLinkProps;
+  container?: NavbarItemContainerProps;
+}>;
+
+export const NavbarItem = ({
+  link,
+  container,
+  children,
+}: NavbarItemProps): JSX.Element => {
+  if (link) {
+    return <NavbarItemLink {...link}>{children}</NavbarItemLink>;
+  } else {
+    return <NavbarItemContainer {...container}>{children}</NavbarItemContainer>;
+  }
 };
