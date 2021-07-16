9f7ca2f5698bda5ac0bde3eb70278af9c0901648
Brendan Tierney <tiernebre@gmail.com>
2021-07-15 21:45:27 -0600

    react hook form register

--- src/components/form/input/Input.test.tsx
+++ src/components/form/input/Input.test.tsx
@@ -1,8 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { colors } from "../../..";
 import { Color, Size, sizes } from "../../../types";
 import { Input } from ".";
 import user from "@testing-library/user-event";
+import { useForm } from "react-hook-form";
+import { Button } from "../../button";
 
 it("is entirely unopinionated if not given any props", () => {
   render(<Input />);
@@ -78,4 +80,46 @@ it("is not displayed as static if provided as false", () => {
   render(<Input isStatic={false} />);
   const input = screen.getByRole("textbox");
   expect(input).not.toHaveClass("is-static");
+});
+
+it("can be registered as a React Hook Form uncontrolled component", async () => {
+  type TestForm = {
+    name: string;
+    index: number;
+  };
+  type TestBedProps = {
+    onSubmit: (data: TestForm) => void;
+  };
+  const TestBed = ({ onSubmit }: TestBedProps): JSX.Element => {
+    const { register, handleSubmit } = useForm<TestForm>();
+
+    const submit = (data: TestForm) => {
+      onSubmit(data);
+    };
+
+    return (
+      <form onSubmit={handleSubmit(submit)}>
+        <Input type="text" register={register("name")} />
+        <Input
+          type="number"
+          register={register("index", {
+            valueAsNumber: true,
+          })}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    );
+  };
+  const onSubmit = jest.fn();
+  render(<TestBed onSubmit={onSubmit} />);
+  const name = "Test Name";
+  const index = 12;
+  user.type(screen.getByRole("textbox"), name);
+  user.type(screen.getByRole("spinbutton"), index.toString());
+  user.click(screen.getByRole("button"));
+  await waitFor(() => expect(onSubmit).toHaveBeenCalled());
+  expect(onSubmit).toHaveBeenCalledWith({
+    name,
+    index,
+  });
 });
