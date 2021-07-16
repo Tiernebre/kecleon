98395b731bfb2c617fc3236f49ba313fd4c64f80
Brendan Tierney <tiernebre@gmail.com>
2021-07-15 21:52:51 -0600

    fix: Test react hook form compatibility (#125)
    * react hook form register
    
    * validated input test
    
    * semantic form field tests

--- src/components/form/semantic-form-field/SemanticFormField.test.tsx
+++ src/components/form/semantic-form-field/SemanticFormField.test.tsx
@@ -1,8 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { useForm } from "react-hook-form";
+import { Button } from "../..";
 import { Icon } from "../../icon";
 import { FormControlProps } from "../form-control";
 import { InputProps } from "../input";
 import { SemanticFormField, SemanticFormFieldProps } from "./SemanticFormField";
+import user from "@testing-library/user-event";
 
 it("displays the given label", () => {
   const id = "test-semantic-form-field";
@@ -87,4 +90,53 @@ it("is formatted to handle an error", () => {
   render(<SemanticFormField {...props} />);
   expect(screen.getByRole("textbox")).toBeInvalid();
   expect(screen.getByText(error.message)).toBeInTheDocument();
+});
+
+it("can be registered as a React Hook Form uncontrolled component", async () => {
+  type TestForm = {
+    name: string;
+    index: number;
+  };
+  type TestBedProps = {
+    onSubmit: (data: TestForm) => void;
+  };
+  const TestBed = ({ onSubmit }: TestBedProps): JSX.Element => {
+    const { register, handleSubmit } = useForm<TestForm>();
+
+    const submit = (data: TestForm) => {
+      onSubmit(data);
+    };
+
+    return (
+      <form onSubmit={handleSubmit(submit)}>
+        <SemanticFormField
+          id="name"
+          input={{ type: "text" }}
+          label="Name"
+          register={register("name")}
+        />
+        <SemanticFormField
+          id="index"
+          input={{ type: "number" }}
+          label="Index"
+          register={register("index", {
+            valueAsNumber: true,
+          })}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    );
+  };
+  const onSubmit = jest.fn();
+  render(<TestBed onSubmit={onSubmit} />);
+  const name = "Test Validated Semantic Form Field Name";
+  const index = 123;
+  user.type(screen.getByRole("textbox"), name);
+  user.type(screen.getByRole("spinbutton"), index.toString());
+  user.click(screen.getByRole("button"));
+  await waitFor(() => expect(onSubmit).toHaveBeenCalled());
+  expect(onSubmit).toHaveBeenCalledWith({
+    name,
+    index,
+  });
 });
