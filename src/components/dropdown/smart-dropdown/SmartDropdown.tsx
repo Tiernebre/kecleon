37d5b714be6908d37cda6781a9a477adc3674dd7
Brendan Tierney <tiernebre@gmail.com>
2021-07-21 22:44:02 -0600

    initial smart dropdown

---
+++ src/components/dropdown/smart-dropdown/SmartDropdown.tsx
@@ -0,0 +1,14 @@
+/**
+ * SmartDropdown is a fully integrated composition of all of the
+ * "dumb" Dropdown components orchestrated togtether using
+ * state and side effects. This makes it easier for consumers
+ * to plug and go with a Dropdown they do not have to assemble
+ * themselves.
+ *
+ * If you want to assemble your own Dropdown or do not want
+ * opinionated state, I suggest using the individual Dropdown
+ * components themselves rather than SmartDropdown.
+ */
+export const SmartDropdown = (): JSX.Element => {
+  return <div>Smart Dropdown</div>;
+};
