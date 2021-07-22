e83866394e394942e144a9c699bd1d6d497add61
Brendan Tierney <tiernebre@gmail.com>
2021-07-21 23:13:57 -0600

    on click binding

--- src/components/dropdown/item/DropdownItem.tsx
+++ src/components/dropdown/item/DropdownItem.tsx
@@ -3,16 +3,18 @@ import { Button } from "../..";
 
 export type DropdownItemProps = PropsWithChildren<{
   active?: boolean;
+  onClick?: () => void;
 }>;
 
 export const DropdownItem = ({
   children,
   active,
+  onClick,
 }: DropdownItemProps): JSX.Element => {
   const color = active ? "link" : "white";
 
   return (
-    <Button color={color} className="dropdown-item">
+    <Button color={color} className="dropdown-item" onClick={onClick}>
       {children}
     </Button>
   );
--- src/components/dropdown/smart-dropdown/SmartDropdown.test.tsx
+++ src/components/dropdown/smart-dropdown/SmartDropdown.test.tsx
@@ -2,6 +2,7 @@ import { screen, render } from "@testing-library/react";
 import { Fragment } from "react";
 import { SmartDropdown } from "./SmartDropdown";
 import user from "@testing-library/user-event";
+import { DropdownItem } from "../item";
 
 it("by default is inactive", () => {
   const triggerLabel = "Smart Dropdown";
@@ -55,4 +56,23 @@ it("properly binds the menu ID between the trigger and the menu itself", () => {
   );
   expect(screen.getByRole("button")).toHaveAttribute("aria-controls", menuId);
   expect(screen.getByRole("menu")).toHaveAttribute("id", menuId);
+});
+
+it("renders given items", () => {
+  const menuId = "expected-menu";
+  const onClick = jest.fn();
+  render(
+    <SmartDropdown
+      menuId={menuId}
+      triggerLabel="Dropdown"
+      items={
+        <Fragment>
+          <DropdownItem>First Option</DropdownItem>
+          <DropdownItem onClick={onClick}>Second Option</DropdownItem>
+        </Fragment>
+      }
+    />
+  );
+  expect(screen.getByRole("button", { name: /first/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /second/i })).toBeInTheDocument();
 });
