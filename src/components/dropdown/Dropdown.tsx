b27e48fe9dee244bffb122dac66640a0aaa0d5c6
Brendan Tierney <tiernebre@gmail.com>
2021-07-21 21:25:24 -0600

    initial dropdown props

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,3 +1,30 @@
-export const Dropdown = (): JSX.Element => {
-  return <div>Dropdown</div>;
+import { PropsWithChildren } from "react";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../utilities";
+
+export type DropdownProps = PropsWithChildren<{
+  hoverable?: boolean;
+  active?: boolean;
+  className?: string;
+}>;
+
+const classNameMapping: ClassNameTransformMap<DropdownProps> = new Map([
+  ["hoverable", () => "is-hoverable"],
+  ["active", () => "is-active"],
+]);
+
+export const Dropdown = ({
+  hoverable,
+  active,
+  className = "",
+  children,
+}: DropdownProps): JSX.Element => {
+  const mappedClassName = createClassNameFromProps(
+    classNameMapping,
+    { hoverable, active } as Partial<DropdownProps>,
+    ["dropdown", className]
+  );
+  return <div className={mappedClassName}>{children}</div>;
 };
