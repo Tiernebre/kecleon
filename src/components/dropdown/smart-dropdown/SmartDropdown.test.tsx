215fea64ecf7f2ed8ea5e8c0c48323fe0f1ffce4
Brendan Tierney <tiernebre@gmail.com>
2021-07-21 23:06:59 -0600

    active setup

--- src/components/dropdown/smart-dropdown/SmartDropdown.test.tsx
+++ src/components/dropdown/smart-dropdown/SmartDropdown.test.tsx
@@ -1,13 +1,33 @@
 import { screen, render } from "@testing-library/react";
 import { Fragment } from "react";
 import { SmartDropdown } from "./SmartDropdown";
+import user from "@testing-library/user-event";
 
 it("by default is inactive", () => {
+  const triggerLabel = "Smart Dropdown";
   render(
     <SmartDropdown
       menuId="Foo"
-      triggerLabel="Dropdown"
+      triggerLabel={triggerLabel}
       items={<Fragment></Fragment>}
     />
+  );
+  expect(
+    screen.getByRole("button").parentElement?.parentElement
+  ).not.toHaveClass("is-active");
+});
+
+it("becomes active when the trigger is clicked", () => {
+  const triggerLabel = "Smart Dropdown";
+  render(
+    <SmartDropdown
+      menuId="Foo"
+      triggerLabel={triggerLabel}
+      items={<Fragment></Fragment>}
+    />
+  );
+  user.click(screen.getByRole("button"));
+  expect(screen.getByRole("button").parentElement?.parentElement).toHaveClass(
+    "is-active"
   );
 });
