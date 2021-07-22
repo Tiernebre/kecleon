8135eb890ab1cf5489a5ba6779f2365b27fb71f7
Brendan Tierney <tiernebre@gmail.com>
2021-07-21 21:30:15 -0600

    hoverable + active

--- src/components/dropdown/Dropdown.test.tsx
+++ src/components/dropdown/Dropdown.test.tsx
@@ -13,3 +13,27 @@ it("can be rendered with a custom class name", () => {
   render(<Dropdown className={customClassName}>{text}</Dropdown>);
   expect(screen.getByText(text)).toHaveClass("some-custom", "class-name");
 });
+
+it("can be hoverable", () => {
+  const text = "Dropdown";
+  render(<Dropdown hoverable>{text}</Dropdown>);
+  expect(screen.getByText(text)).toHaveClass("is-hoverable");
+});
+
+it("can be non-hoverable", () => {
+  const text = "Dropdown";
+  render(<Dropdown hoverable={false}>{text}</Dropdown>);
+  expect(screen.getByText(text)).not.toHaveClass("is-hoverable");
+});
+
+it("can be active", () => {
+  const text = "Dropdown";
+  render(<Dropdown active>{text}</Dropdown>);
+  expect(screen.getByText(text)).toHaveClass("is-active");
+});
+
+it("can be inactive", () => {
+  const text = "Dropdown";
+  render(<Dropdown active={false}>{text}</Dropdown>);
+  expect(screen.getByText(text)).not.toHaveClass("is-active");
+});
