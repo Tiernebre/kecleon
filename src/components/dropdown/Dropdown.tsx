a4958dd781e4488da4a78145ea5a2d1aeaeb33dc
Brendan Tierney <tiernebre@gmail.com>
2021-07-21 21:26:43 -0600

    dropdown up

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -10,24 +10,27 @@ export type DropdownProps = PropsWithChildren<{
   active?: boolean;
   className?: string;
   alignment?: Direction;
+  up?: boolean;
 }>;
 
 const classNameMapping: ClassNameTransformMap<DropdownProps> = new Map([
   ["hoverable", () => "is-hoverable"],
   ["active", () => "is-active"],
   ["alignment", (alignment: string) => `is-${alignment}`],
+  ["up", () => "is-up"],
 ]);
 
 export const Dropdown = ({
   hoverable,
   active,
   alignment,
+  up,
   className = "",
   children,
 }: DropdownProps): JSX.Element => {
   const mappedClassName = createClassNameFromProps(
     classNameMapping,
-    { hoverable, active, alignment } as Partial<DropdownProps>,
+    { hoverable, active, alignment, up } as Partial<DropdownProps>,
     ["dropdown", className]
   );
   return <div className={mappedClassName}>{children}</div>;
