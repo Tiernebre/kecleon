73f0eb40df505d5b8ceccd3847018dda7808456b
Brendan Tierney <tiernebre@gmail.com>
2021-07-22 21:35:49 -0600

    click outside setup

--- src/hooks/events/use-click-outside.tsx
+++ src/hooks/events/use-click-outside.tsx
@@ -1,20 +1,27 @@
 import { RefObject, useEffect } from "react";
 
+const checkIfClickOutsideOccurredForElement =
+  (element: HTMLElement | null, callback: () => void) =>
+  (event: MouseEvent) => {
+    const elementClickedOn = event.target as Element | null;
+    if (element && elementClickedOn && !element.contains(elementClickedOn)) {
+      callback();
+    }
+  };
+
 export const useClickOutside = (
-  ref: RefObject<HTMLDivElement>,
+  ref: RefObject<HTMLElement>,
   onClickOutside: () => void
 ): void => {
   useEffect(() => {
-    document.addEventListener("click", (event) => {
-      const elementClickedOn = event.target as Element | null;
-      const elementToCheck = ref.current;
-      if (
-        elementClickedOn &&
-        elementToCheck &&
-        !elementToCheck.contains(elementClickedOn)
-      ) {
-        onClickOutside();
-      }
-    });
+    const callback = checkIfClickOutsideOccurredForElement(
+      ref.current,
+      onClickOutside
+    );
+    document.addEventListener("click", callback);
+
+    return () => {
+      document.removeEventListener("click", callback);
+    };
   }, [ref, onClickOutside]);
 };
