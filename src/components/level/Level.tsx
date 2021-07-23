c030bf3a06e1ffcf6950645f31e808852c29adc3
Brendan Tierney <tiernebre@gmail.com>
2021-07-22 22:23:45 -0600

    children

--- src/components/level/Level.tsx
+++ src/components/level/Level.tsx
@@ -1,3 +1,30 @@
-export const Level = (): JSX.Element => {
-  return <div>Level</div>;
+import { PropsWithChildren, ReactNode } from "react";
+import {
+  ClassNameTransformMap,
+  createClassNameFromProps,
+} from "../../utilities";
+
+export type LevelProps = PropsWithChildren<{
+  left?: ReactNode;
+  right?: ReactNode;
+  mobile?: boolean;
+}>;
+
+const classNameMap: ClassNameTransformMap<LevelProps> = new Map([
+  ["mobile", () => "is-mobile"],
+]);
+
+export const Level = ({
+  left,
+  right,
+  children,
+  mobile,
+}: LevelProps): JSX.Element => {
+  const className = createClassNameFromProps(
+    classNameMap,
+    { mobile } as Partial<LevelProps>,
+    ["level"]
+  );
+
+  return <div className={className}>{children}</div>;
 };
