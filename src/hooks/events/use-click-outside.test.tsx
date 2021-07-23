ab2e309f57ffdf5b79d4ba0f6afd4510f72b6482
Brendan Tierney <tiernebre@gmail.com>
2021-07-22 21:44:03 -0600

    inital callback test

---
+++ src/hooks/events/use-click-outside.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { useRef } from "react";
+import { useClickOutside } from "./use-click-outside";
+import user from "@testing-library/user-event";
+
+type ClickOutsideTestBedProps = {
+  onClickOutside: () => void;
+};
+
+const ClickOutsideTestBed = ({
+  onClickOutside,
+}: ClickOutsideTestBedProps): JSX.Element => {
+  const elementToCheck = useRef<HTMLDivElement>(null);
+  useClickOutside(elementToCheck, onClickOutside);
+
+  return (
+    <div>
+      <div data-testid="click-outside-listener" ref={elementToCheck}>
+        Anything that gets clicked outside of this should trigger something!
+        <span>Even a child element!</span>
+      </div>
+      <div data-testid="click-outside-trigger">
+        Click me to trigger a click outside
+      </div>
+    </div>
+  );
+};
+
+it("calls the given callback if a click outside occurred outside of the provided element", async () => {
+  const onClickOutside = jest.fn();
+  render(<ClickOutsideTestBed onClickOutside={onClickOutside} />);
+  user.click(screen.getByTestId("click-outside-trigger"));
+  await waitFor(() => expect(onClickOutside).toHaveBeenCalledTimes(1));
+});
