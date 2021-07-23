351ca5aea2259cf0adb0ec9b190556ffba571165
Brendan Tierney <tiernebre@gmail.com>
2021-07-22 22:26:12 -0600

    mobile test

--- src/components/level/Level.test.tsx
+++ src/components/level/Level.test.tsx
@@ -1,9 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import React from "react";
 import { Level } from ".";
 
 it("by default has unopinionated styling", () => {
   const levelText = "Level";
   render(<Level>{levelText}</Level>);
   expect(screen.getByText(levelText)).toHaveClass("level", { exact: true });
+});
+
+it("supports mobile", () => {
+  const levelText = "Level";
+  render(<Level mobile>{levelText}</Level>);
+  expect(screen.getByText(levelText)).toHaveClass("is-mobile");
+});
+
+it("can specifically not support mobile", () => {
+  const levelText = "Level";
+  render(<Level mobile={false}>{levelText}</Level>);
+  expect(screen.getByText(levelText)).not.toHaveClass("is-mobile");
 });
