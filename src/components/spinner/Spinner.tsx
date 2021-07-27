8d32e73e48e103aa6af505df8a4b220545d6e447
Brendan Tierney <tiernebre@gmail.com>
2021-07-26 19:36:05 -0600

    spinner dots

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,12 +1,17 @@
+import styles from "./Spinner.module.css";
+
+export const SpinnerDot = (): JSX.Element => (
+  <div className={styles.spinner_dot} aria-hidden="true" />
+);
+
 export const Spinner = (): JSX.Element => {
   return (
-    <div className="sk-chase">
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
+    <div className={styles.spinner}>
+      <SpinnerDot />
+      <SpinnerDot />
+      <SpinnerDot />
+      <SpinnerDot />
+      <SpinnerDot />
     </div>
   );
 };
