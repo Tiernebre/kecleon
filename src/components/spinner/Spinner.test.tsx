26da8d86ac22d9fa868770f19f5b41c92bbd6717
Brendan Tierney <tiernebre@gmail.com>
2021-07-26 20:37:46 -0600

    custom color mappings

--- src/components/spinner/Spinner.test.tsx
+++ src/components/spinner/Spinner.test.tsx
@@ -1,5 +1,6 @@
 import { screen, render } from "@testing-library/react";
 import { Spinner } from ".";
+import { Color, colors } from "../../types";
 import styles from "./Spinner.module.scss";
 
 it("by default is white", () => {
@@ -18,4 +19,9 @@ it("supports a custom label", () => {
   render(<Spinner label={label} />);
   const spinner = screen.getByRole("alert");
   expect(spinner).toHaveAttribute("aria-label", label);
+});
+
+it.each<Color>(colors)("can be colored in %p", (color: Color) => {
+  render(<Spinner color={color} />);
+  expect(screen.getByRole("alert")).toHaveClass(styles[`is-${color}`]);
 });
