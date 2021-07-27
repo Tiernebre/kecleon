27a4274c43b3722222745ddbd2777e6df29e7583
Brendan Tierney <tiernebre@gmail.com>
2021-07-26 20:36:09 -0600

    accessible label tests

--- src/components/spinner/Spinner.test.tsx
+++ src/components/spinner/Spinner.test.tsx
@@ -6,3 +6,16 @@ it("by default is white", () => {
   render(<Spinner />);
   expect(screen.getByRole("alert")).toHaveClass(styles["is-white"]);
 });
+
+it("by default is labeled with 'Loading...'", () => {
+  render(<Spinner />);
+  const spinner = screen.getByRole("alert");
+  expect(spinner).toHaveAttribute("aria-label", "Loading...");
+});
+
+it("supports a custom label", () => {
+  const label = "Loading Test...";
+  render(<Spinner label={label} />);
+  const spinner = screen.getByRole("alert");
+  expect(spinner).toHaveAttribute("aria-label", label);
+});
