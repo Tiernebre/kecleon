445ec13811cbeb319eb47fadc0e4a427244cbd58
Brendan Tierney <tiernebre@gmail.com>
2021-07-28 22:16:25 -0600

    omit CSS from page spinner

--- src/components/page-spinner/PageSpinner.tsx
+++ src/components/page-spinner/PageSpinner.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { Spinner, SpinnerProps } from "..";
 import styles from "./PageSpinner.module.scss";
 
-export type PageSpinnerProps = SpinnerProps;
+export type PageSpinnerProps = Omit<SpinnerProps, "css">;
 
 export const PageSpinner = (props: PageSpinnerProps): JSX.Element => {
   const spinnerRef = useRef<HTMLDivElement>(null);
